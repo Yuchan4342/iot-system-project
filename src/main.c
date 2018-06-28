76cd0359ecb3915d9668021830fc0a68dabdb785
Yuto Iwata <jyuchan415@gmail.com>
2018-06-28 16:50:07 +0900

    動作のひな形作成

--- src/main.c
+++ src/main.c
@@ -3,10 +3,18 @@
 void sendPattern();
 
 int main(int argc, char* argv[]) {
-
+	sendPattern();
+	run();
 	return 0;
 }
 
+// 光るタイミングとパターンをボードに送信する
 void sendPattern() {
-	
+	printf("Sending pattern when LEDs flash.");
+
+}
+
+// ゲーム本番
+void run() {
+	printf("Game Start!\n");
 }
