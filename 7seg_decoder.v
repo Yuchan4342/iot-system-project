fadf80786b5b6eaa901c9deb80e198b7aa8d27ac
Yuto Iwata <jyuchan415@gmail.com>
2018-07-12 17:36:39 +0900

    7セグの出力修正.

--- 7seg_decoder.v
+++ 7seg_decoder.v
@@ -26,16 +26,16 @@ module nanaseg_decoder (
   function [11:0] seg_decode;
     input [3:0] d_digit;
     case(d_digit)
-      4'd0: seg_decode = 12'b011001001011;
-      4'd1: seg_decode = 12'b000001001000;
-      4'd2: seg_decode = 12'b010001010011;
-      4'd3: seg_decode = 12'b010001011010;
-      4'd4: seg_decode = 12'b001001011000;
-      4'd5: seg_decode = 12'b011000011010;
-      4'd6: seg_decode = 12'b011000011011;
-      4'd7: seg_decode = 12'b011001001000;
-      4'd8: seg_decode = 12'b011001011011;
-      4'd9: seg_decode = 12'b011001011010;
+      4'd0: seg_decode = 12'b000000010100;
+      4'd1: seg_decode = 12'b011000010111;
+      4'd2: seg_decode = 12'b001000001100;
+      4'd3: seg_decode = 12'b001000000101;
+      4'd4: seg_decode = 12'b010000000111;
+      4'd5: seg_decode = 12'b000001000101;
+      4'd6: seg_decode = 12'b000001000100;
+      4'd7: seg_decode = 12'b000000010111;
+      4'd8: seg_decode = 12'b000000000100;
+      4'd9: seg_decode = 12'b000000000101;
       default: seg_decode = 12'b000000000000;
     endcase
   endfunction
